5cfe8919cc5642d419c46e5b7b22af05f04b8c06
Dariel Ramos <dariel.ramos.ddv@gmail.com>
2023-08-27 23:47:28 -0400

    add singleton to ApiEndPoints

--- lib/data/remote/api_end_points.dart
+++ lib/data/remote/api_end_points.dart
@@ -1,3 +1,9 @@
 class ApiEndPoints {
   final String getNewsList = 'v1/news';
+
+  static final ApiEndPoints _singleton = ApiEndPoints._internal();
+
+  factory ApiEndPoints() => _singleton;
+
+  ApiEndPoints._internal();
 }
